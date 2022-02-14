141dcd9e4334b8322f5b6a998a68bb6a60a20f27
VISHAL <1316510155@kit.ac.in>
2022-02-14 12:18:46 +0530

    2

--- app/src/main/java/com/example/thescan/MainActivity.kt
+++ app/src/main/java/com/example/thescan/MainActivity.kt
@@ -1,11 +1,118 @@
 package com.example.thescan
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.security.Permissions
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    var photoFile: File? = null
+    val CAPTURE_IMAGE_REQUEST = 1
+    var mCurrentPhotoPath: String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn =findViewById<Button>(R.id.click)
+
+
+
+
+        btn.setOnClickListener {
+            takePicture()
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun takePicture() {
+        if (ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            0
+        )
+    } else {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            // Create the File where the photo should go
+            try {
+                photoFile = createImageFile()
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    val photoURI = FileProvider.getUriForFile(
+                        this,
+                        "com.example.thescan.fileprovider",
+                        photoFile!!
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST)
+                }
+            } catch (ex: Exception) {
+                // Error occurred while creating the File
+                displayMessage(baseContext, ex.message.toString())
+            }
+
+        } else {
+            displayMessage(baseContext, "Null")
+        }
+    }
+
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName, /* prefix */
+            ".jpg", /* suffix */
+            storageDir      /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.absolutePath
+        return image
+    }
+
+    private fun displayMessage(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val img_view=findViewById<ImageView>(R.id.img_view)
+        if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            val myBitmap = BitmapFactory.decodeFile(photoFile!!.absolutePath)
+            img_view.setImageBitmap(myBitmap)
+        } else {
+            displayMessage(baseContext, "Request cancelled or something went wrong.")
+        }
     }
 }
